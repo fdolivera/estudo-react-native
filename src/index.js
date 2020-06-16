50d67c791c5568d8f1ad16938f4a46acfe47cc1a
Felipe <felipe.olivera_@outlook.com>
2020-06-16 20:35:38 -0300

    Header and Suggestions component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
 
 import Navigation from '../src/Navigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#000"/>
+      <NavigationContainer>
         <Navigation />
-    </NavigationContainer>
+      </NavigationContainer>
+    </>
   );
 }
